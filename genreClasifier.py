a06b8966180544e3d61ff9dd0679c23f19f83fc5
Moonwalker <abhisk.mhra@gmail.com>
2021-03-21 21:27:35 +0530

    Simple MLP training for genre classification

--- genreClasifier.py
+++ genreClasifier.py
@@ -11,19 +11,22 @@ from keras.layers import Flatten
 
 # function for loading the dataset
 def load_data(dataset_path):
-    with open(dataset_path,'r')  as fp:
+    with open(dataset_path,'r') as fp:
         data = json.load(fp)
-
-        # converting lists to np.arrays
+        
+        
+        ## convert lists into np.arrays
+        
         inputs = np.array(data['mfcc'])
         targets = np.array(data['label'])
-
+        
         return inputs, targets
 
 # function for training MLP
-def train_mlp(inputs,targets):
+def train_mlp(inputs,targets,n_epochs=60):
     optimizer = Adam(learning_rate=0.0001)
-    X_train, y_train, X_test, y_test = train_test_split(inputs,targets)
+    
+    X_train, X_test, y_train, y_test = train_test_split(inputs,targets,test_size=0.33)
     model = Sequential([
         
         # input layer
@@ -37,11 +40,17 @@ def train_mlp(inputs,targets):
         # output layer with 10 neurons
         Dense(10,activation='softmax'),
     ])
-    model.compile(optimizers=optimizer,metrics=['accuracy'],loss='sparse_categorical_crossentropy')
+    model.compile(optimizer=optimizer,
+    metrics=['accuracy'],
+    loss='sparse_categorical_crossentropy')
     print("Model Summary : \n",model.summary())
+    history = model.fit(X_train,y_train,
+    validation_data=(X_test,y_test),
+    epochs=n_epochs)
 
 
 
 if __name__ == '__main__':
     inputs, targets = load_data('data.json')
     train_mlp(inputs, targets)
+    
