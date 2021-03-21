d22abe2a396aeec5a3b3aa5acddb9f2a647a7c04
Moonwalker <abhisk.mhra@gmail.com>
2021-03-21 20:39:47 +0530

    wip: Implementing MLP training for Music Genre Classification

---
+++ genreClasifier.py
@@ -0,0 +1,47 @@
+# importing modules 
+import json
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.layers import Flatten
+
+
+
+# function for loading the dataset
+def load_data(dataset_path):
+    with open(dataset_path,'r')  as fp:
+        data = json.load(fp)
+
+        # converting lists to np.arrays
+        inputs = np.array(data['mfcc'])
+        targets = np.array(data['label'])
+
+        return inputs, targets
+
+# function for training MLP
+def train_mlp(inputs,targets):
+    optimizer = Adam(learning_rate=0.0001)
+    X_train, y_train, X_test, y_test = train_test_split(inputs,targets)
+    model = Sequential([
+        
+        # input layer
+        Flatten(input_shape=(inputs.shape[1],inputs.shape[2])),
+        
+        # hidden layers
+        Dense(512,activation='relu'),
+        Dense(256,activation='relu'),
+        Dense(64,activation='relu'),
+      
+        # output layer with 10 neurons
+        Dense(10,activation='softmax'),
+    ])
+    model.compile(optimizers=optimizer,metrics=['accuracy'],loss='sparse_categorical_crossentropy')
+    print("Model Summary : \n",model.summary())
+
+
+
+if __name__ == '__main__':
+    inputs, targets = load_data('data.json')
+    train_mlp(inputs, targets)
